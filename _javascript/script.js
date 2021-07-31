475906032fd7b586ed3a8d43ca7c227855d501af
Maria Kauffmann <mariatkauffmann@gmail.com>
2021-07-31 15:31:10 -0300

    header com animação e menu hamburger

---
+++ _javascript/script.js
@@ -0,0 +1,8 @@
+//animação nav e ícone hamburger
+let navMobile = document.querySelector(".nav-links");
+let hamburger = document.querySelector(".mobile-nav-icon")
+
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("open");
+    navMobile.classList.toggle("closed");
+})
