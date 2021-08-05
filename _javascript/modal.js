3a200c8d4d6d6ab3dec86e17198b40e378a068d3
Maria Kauffmann <mariatkauffmann@gmail.com>
2021-08-05 20:39:49 -0300

    página de login e modal

---
+++ _javascript/modal.js
@@ -0,0 +1,8 @@
+function abrirModal(){
+    document.getElementById('modal-senai').style.top="0";
+    document.getElementById('modal-senai').style.display="block";
+}
+function fecharModal(){
+    document.getElementById('modal-senai').style.top="-100%";
+    document.getElementById('modal-senai').style.display = "none";
+}
