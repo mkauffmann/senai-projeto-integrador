3579cc7199961bfb4143904918fd460c2951ea2a
Carlos Tadeu <carlostadeu.dev@gmail.com>
2025-05-05 21:11:53 -0300

    mudancas basicas

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,48 +1,44 @@
 <template>
-  <footer class="mt-5">
-    <div class="footer-container">
-      <div class="footer-info">
-        <img
-          src="@/assets/images/logo_CB-03.png"
-          alt="Ciclo Básico"
-          class="footer-logo mb-3"
-        />
-        <p><i class="bi bi-geo-alt-fill"></i> R. Gandavo, 550 - Vila Mariana, 
-          São Paulo - SP</p>
-        <p><i class="bi bi-envelope-fill"></i> contato@ciclobasico.com.br</p>
-        <p><i class="bi bi-telephone-fill"></i> (11) 5555-5555</p>
-        <ul class="redes-sociais redes-sociais-footer">
-          <li><a href="#"><i class="bi bi-facebook"></i></a></li>
-          <li><a href="#"><i class="bi bi-instagram"></i></a></li>
-          <li><a href="#"><i class="bi bi-twitter"></i></a></li>
-          <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
-        </ul> 
-      </div>
-      <div class="footer-nav">
-        <ul class="footer-nav-items">
-          <div class="footer-nav-links">
-            <li class="nav-item"><router-link to="/catalogo">Catálogo</router-link></li>
-            <li class="nav-item"><router-link to="/sobre">Sobre</router-link></li>
-            <li class="nav-item"><router-link to="/contato">Contato</router-link></li>
-            <li class="nav-item"><router-link to="/login">Entrar</router-link></li>
-          </div>
-          <li class="nav-item busca-footer">
-            <input
-              class="busca"
-              type="text"
-              name="busca"
-              placeholder="Pesquisar"
-            />
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="footer-copy d-none d-sm-block mt-5">
-      <em>
-        2023 - Projeto Integrador - Curso Técnico de Informática para Internet
-        - SENAI/SP</em>
-    </div>
-  </footer>
+	<footer class="mt-5">
+		<div class="footer-container">
+			<div class="footer-info">
+				<img src="@/assets/images/logoBranca.png" alt="Eduteck" class="footer-logo mb-3" />
+				<p><i class="bi bi-geo-alt-fill"></i> R. Gandavo, 550 - Vila Mariana, São Paulo - SP</p>
+				<p><i class="bi bi-envelope-fill"></i> contato@ciclobasico.com.br</p>
+				<p><i class="bi bi-telephone-fill"></i> (11) 5555-5555</p>
+				<ul class="redes-sociais redes-sociais-footer">
+					<li>
+						<a href="#"><i class="bi bi-facebook"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="bi bi-instagram"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="bi bi-twitter"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="bi bi-linkedin"></i></a>
+					</li>
+				</ul>
+			</div>
+			<div class="footer-nav">
+				<ul class="footer-nav-items">
+					<div class="footer-nav-links">
+						<li class="nav-item"><router-link to="/catalogo">Catálogo</router-link></li>
+						<li class="nav-item"><router-link to="/sobre">Sobre</router-link></li>
+						<li class="nav-item"><router-link to="/contato">Contato</router-link></li>
+						<li class="nav-item"><router-link to="/login">Entrar</router-link></li>
+					</div>
+					<li class="nav-item busca-footer">
+						<input class="busca" type="text" name="busca" placeholder="Pesquisar" />
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer-copy d-none d-sm-block mt-5">
+			<em> Eduteck - PUC-Minas</em>
+		</div>
+	</footer>
 </template>
 
 <script setup lang="ts">
@@ -51,4 +47,4 @@
 
 <style scoped>
 /* Os estilos agora vêm do arquivo shared.css */
-</style> 
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,53 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">
-      <img
-        class="nav-logo"
-        src="@/assets/images/logo_CB-03.png"
-        alt="Ciclo Básico"
-      />
-    </router-link>
+	<nav>
+		<router-link to="/">
+			<img class="nav-logo" src="@/assets/images/logoEduteck.png" alt="Eduteck" />
+		</router-link>
 
-    <ul class="nav-items">
-      <div class="nav-links" :class="{ closed: !isMobileMenuOpen }">
-        <li class="nav-item"><router-link to="/catalogo">Catálogo</router-link></li>
-        <li class="nav-item"><router-link to="/sobre">Sobre</router-link></li>
-        <li class="nav-item"><router-link to="/contato">Contato</router-link></li>
-        <li class="nav-item login login-mobile"><router-link to="/login">Entrar</router-link></li>
-      </div>
-      <li class="nav-item busca-container">
-        <input
-          class="busca custom-input"
-          type="text"
-          name="busca"
-          placeholder="Pesquisar"
-        />
-      </li>
-      <router-link to="/login"><li class="nav-item login login-desktop">Entrar</li></router-link>
-    </ul>
-    <div class="mobile-nav-icon" :class="{ closed: !isMobileMenuOpen, open: isMobileMenuOpen }" @click="toggleMobileMenu">
-      <span class="hamburger hamburger-1"></span>
-      <span class="hamburger hamburger-2"></span>
-      <span class="hamburger hamburger-3"></span>
-    </div>
-  </nav>
+		<ul class="nav-items">
+			<div class="nav-links" :class="{closed: !isMobileMenuOpen}">
+				<li class="nav-item"><router-link to="/catalogo">Catálogo</router-link></li>
+				<li class="nav-item"><router-link to="/sobre">Sobre</router-link></li>
+				<li class="nav-item"><router-link to="/contato">Contato</router-link></li>
+				<li class="nav-item login login-mobile">
+					<router-link to="/login">Entrar</router-link>
+				</li>
+			</div>
+			<li class="nav-item busca-container">
+				<input class="busca custom-input" type="text" name="busca" placeholder="Pesquisar" />
+			</li>
+			<router-link to="/login"><li class="nav-item login login-desktop">Entrar</li></router-link>
+		</ul>
+		<div
+			class="mobile-nav-icon"
+			:class="{closed: !isMobileMenuOpen, open: isMobileMenuOpen}"
+			@click="toggleMobileMenu"
+		>
+			<span class="hamburger hamburger-1"></span>
+			<span class="hamburger hamburger-2"></span>
+			<span class="hamburger hamburger-3"></span>
+		</div>
+	</nav>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import {ref} from "vue";
 
 const isMobileMenuOpen = ref(false);
 
 function toggleMobileMenu() {
-  isMobileMenuOpen.value = !isMobileMenuOpen.value;
+	isMobileMenuOpen.value = !isMobileMenuOpen.value;
 }
 </script>
 
 <style scoped>
 /* Garantir que não haja conflitos de estilo */
 nav {
-  position: sticky;
-  top: 0;
-  z-index: 1000;
+	position: sticky;
+	top: 0;
+	z-index: 1000;
 }
-</style> 
+</style>
