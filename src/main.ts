c20a927f893295494e0ff7c3ff8de09cb66138a8
Marcus Xavier <marcusxavierr123@gmail.com>
2025-05-10 11:08:11 -0300

    feat: add axios and pinia dependencies to project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
@@ -15,6 +16,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+const pinia = createPinia()
+
 createApp(App)
   .use(router)
+  .use(pinia)
   .mount('#app')
